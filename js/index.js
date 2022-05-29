332106a823ad2f646769cb1d7bcb169e0b7a705c
R. Fariz <farizramazanov38@gmail.com>
2022-05-30 00:08:54 +0400

    feat: add local storage

--- js/index.js
+++ js/index.js
@@ -38,3 +38,50 @@ accordionHeads.forEach((accordion) => {
 });
 
 //************** accordion home page end ******************* */
+
+//************** card start *****************/
+
+const cardBtns = document.querySelectorAll('.card__btn');
+
+let roomDesigns = [];
+
+
+cardBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    
+    const roomDesignsItem = {};
+    roomDesignsItem.price = btn.previousElementSibling.getAttribute('data-price');
+    roomDesignsItem.name = btn.previousElementSibling.innerText;
+    roomDesignsItem.category = btn.parentElement.parentElement.parentElement.previousElementSibling.innerText;
+    roomDesigns.push(roomDesignsItem);
+    const roomDesignsObj = JSON.stringify(roomDesigns);
+    localStorage.setItem("roomDesign", roomDesignsObj);
+    showCardItem();
+    window.addEventListener('load', () => {
+      roomDesigns = getDesignsFromLocalStorage()
+    })
+  })
+});
+
+let cardList = document.querySelector('.card-list');
+
+function showCardItem() {
+  const design = localStorage.getItem("roomDesign");
+  const parsedObj = JSON.parse(design);
+  let li = document.createElement('li');
+
+  for (let index = 0; index < parsedObj.length; index++) {
+    li.innerText = parsedObj[index].category + ' ' + parsedObj[index].price;
+    cardList.append(li)
+  }
+}
+
+function getDesignsFromLocalStorage() {
+  const design = localStorage.getItem("roomDesign");
+  const parsedObj = JSON.parse(design);
+  return parsedObj;
+}
+
+// showCardItem()
+
+//************** card end *****************/
